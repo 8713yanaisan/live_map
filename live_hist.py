bf248cc2e1bc1d2d82d5fe7914ce7dec28cf585e
yaanai <128979745+8713yanaisan@users.noreply.github.com>
2023-05-04 10:06:55 +0900

    Add files via upload

--- live_hist.py
+++ live_hist.py
@@ -19,9 +19,26 @@ st.title("ライブ情報のダッシュボード")
     
 #df=pd.read_excel(r"D:\Pydoc\MyPythonScripts\streamlit_webapp\live_info\Live info.xlsx")
 df=pd.read_excel("Live info.xlsx")
-
+df=df.sort_values("date", ascending=False)
 df["year"]=df["date"].str[:4]
 
+
+#artistのデータフレーム
+df_0=df.loc[:,['artist', 'date',"year","title"]]
+
+df_1=df.loc[:,['artist1', 'date',"year","title"]]
+df_1=df_1.rename(columns={"artist1":"artist"})
+df_1=df_1.dropna(subset=['artist'])
+
+df_2=df.loc[:,['artist2', 'date',"year","title"]]
+df_2=df_2.rename(columns={"artist2":"artist"})
+df_2=df_2.dropna(subset=['artist'])
+
+df_3=df.loc[:,['artist3', 'date',"year","title"]]
+df_3=df_3.rename(columns={"artist3":"artist"})
+df_3=df_3.dropna(subset=['artist'])
+
+df_artist = pd.concat([df_0, df_1,df_2,df_3], ignore_index=True)
 
 
 #列名のリスト
@@ -30,20 +47,28 @@ clm=list(df.columns)
 df
 
 #集計結果表示
-df_groupby = df["artist"].value_counts()
+df_groupby = df_artist["artist"].value_counts()
 df_groupby
 
 df_year=df["year"].value_counts()
 df_year
 
-#TimeStampの変換するかどうか
+#フィルターするかどうか
 filter_check = st.checkbox('フィルターしますか')
 if filter_check:
     fil=st.selectbox('filter',clm)
     'filterは',fil,'です。'
-    selected_erea = st.multiselect('グラフに表示する要素を選択', list(set(list(df[fil]))))
-    df = df[(df[fil].isin(selected_erea))]
+    selected_erea = st.multiselect('表示する要素を選択', list(set(list(df[fil]))))
+    df_fil = df[(df[fil].isin(selected_erea))]
 
+df_fil
+
+#集計結果表示
+df_groupby_fil = df_fil["artist"].value_counts()
+df_groupby_fil
+
+df_year_fil=df_fil["year"].value_counts()
+df_year_fil
 
 st.bar_chart(df_year)
 
